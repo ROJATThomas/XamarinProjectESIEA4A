3e164b1acd4480dd873ce1b727f17b0a8da01999
ROJATThomas <rojat@et.esiea.fr>
2017-12-25 22:35:58 +0100

    Ajoutez des fichiers projet.

---
+++ App2/App2/Models/CustomerDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Realms;
+
+namespace App2.Models
+{
+    public class CustomerDetails : RealmObject
+    {
+        [PrimaryKey]
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int CustomerAge { get; set; }
+    }
+}
